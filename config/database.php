c391fcf0901c0bdb033da76cca29574675ebb33e
PatrissolKenfack <patrissolkenfack@gmail.com>
2019-04-29 05:28:50 +0100

    Procfile for heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,18 @@
 <?php
 
+	/*
+	|--------------------------------------------------------------------------
+	| Information about database
+	|--------------------------------------------------------------------------
+	|
+	| Here we are getting all the information about our database by just taking
+	| them into .env .
+	|
+	|--------------------------------------------------------------------------
+	*/
+
+$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+
 return [
 
     /*
@@ -13,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,16 +71,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
